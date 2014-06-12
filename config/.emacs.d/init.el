cf0a8df50fe4d4f52de3ad92527d579451344a96
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-06-12 15:35:27 +0300

    Increase font size

--- config/.emacs.d/init.el
+++ config/.emacs.d/init.el
@@ -234,5 +234,5 @@ This will call Python in the background and initialize
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 113 :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#2e3434" :foreground "#eeeeec" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
 
