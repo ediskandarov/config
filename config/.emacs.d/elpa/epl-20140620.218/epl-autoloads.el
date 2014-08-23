ebc64a13ae7e330544d5000285778d5f76fa2d2e
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-08-23 22:40:57 +0300

    Update elpa

--- config/.emacs.d/elpa/epl-20140620.218/epl-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("epl-pkg.el" "epl.el") (21417 34921 72591
-;;;;;;  991000))
-
-;;;***
-
-(provide 'epl-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; epl-autoloads.el ends here
