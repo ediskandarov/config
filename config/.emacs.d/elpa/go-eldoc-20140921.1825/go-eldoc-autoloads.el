6044e24988603410aa0575bc8d0858ed5f3666eb
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-09-26 21:26:48 +0300

    Update ELPA

---
+++ config/.emacs.d/elpa/go-eldoc-20140921.1825/go-eldoc-autoloads.el
@@ -0,0 +1,29 @@
+;;; go-eldoc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (go-eldoc-setup) "go-eldoc" "go-eldoc.el" (21541
+;;;;;;  44866 846696 963000))
+;;; Generated autoloads from go-eldoc.el
+
+(autoload 'go-eldoc-setup "go-eldoc" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("go-eldoc-pkg.el") (21541 44866 867800
+;;;;;;  12000))
+
+;;;***
+
+(provide 'go-eldoc-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; go-eldoc-autoloads.el ends here
