9637bb5a82d83d727e53bf5103f0c00addabca5d
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-11-30 15:02:18 +0200

    Update elpa

--- config/.emacs.d/elpa/web-mode-20141112.2327/web-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; web-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21608 35258
-;;;;;;  644085 510000))
-;;; Generated autoloads from web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("web-mode-pkg.el") (21608 35258 676097
-;;;;;;  347000))
-
-;;;***
-
-(provide 'web-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; web-mode-autoloads.el ends here
