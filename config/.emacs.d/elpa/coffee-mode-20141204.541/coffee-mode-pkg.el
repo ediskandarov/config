7f95bd391d9601d07f8a4a1d036a6056d4eb36ba
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-12-06 17:09:16 +0200

    Update ELPA

---
+++ config/.emacs.d/elpa/coffee-mode-20141204.541/coffee-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "coffee-mode" "20141204.541" "Major mode to edit CoffeeScript files in Emacs" (quote ((emacs "24.1") (cl-lib "0.5"))))
---
+++ config/.emacs.d/elpa/web-mode-20141204.1436/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20141204.1436" "major mode for editing html templates" (quote nil))
