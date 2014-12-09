c58a545b78fb2517097cf8847f6304f533edaa2e
Andrew Svetlov <andrew.svetlov@gmail.com>
2014-12-09 21:09:50 +0200

    Update ELPA

--- config/.emacs.d/elpa/dash-20141201.2206/dash-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21635 7168 221805
-;;;;;;  775000))
-
-;;;***
-
-(provide 'dash-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dash-autoloads.el ends here
--- config/.emacs.d/elpa/web-mode-20141204.1436/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20141204.1436" "major mode for editing html templates" (quote nil))
