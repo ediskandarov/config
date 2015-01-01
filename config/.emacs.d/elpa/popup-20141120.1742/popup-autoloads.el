6d88badcc7689b6c6bff33d24d601529cee2f4c7
Andrew Svetlov <andrew.svetlov@gmail.com>
2015-01-01 15:03:03 +0200

    Update ELPA

--- config/.emacs.d/elpa/popup-20141120.1742/popup-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; popup-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21627 5181
-;;;;;;  665321 932000))
-
-;;;***
-
-(provide 'popup-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; popup-autoloads.el ends here
---
+++ config/.emacs.d/elpa/web-mode-20141231.1631/web-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "web-mode" "20141231.1631" "major mode for editing web templates" (quote nil))
